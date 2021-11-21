139ea20b4cb6f2d92e9bb737703d4c117077d0b9
YashKharola <91362319+YashKharola@users.noreply.github.com>
2021-11-21 11:51:05 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,sum=0;
+float avg;
+cout<<"Enter the value of a and b";
+cin>>a>>b;
+ sum=a+b;
+ avg=sum/2;
+ cout<<"avg is\n"<<avg;
+}
